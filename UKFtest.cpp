892973040f06b537b600360d7bfae15e762c8e09
Neil Wagner <114113746+nrwagner29@users.noreply.github.com>
2025-06-18 10:00:43 -0500

    Update UKFtest.cpp

--- UKFtest.cpp
+++ UKFtest.cpp
@@ -1,9 +1,8 @@
 #include "lib/main.hpp"
 #include <iostream>
 
-
 int main() {
-    UKF ukf;
+UKF ukf;
     x << 1,
             0,
             0,
@@ -12,19 +11,12 @@ int main() {
             0,
             0;
 
-        P << .05, 0, 0, 0, 0, 0,
-            0, .05, 0, 0, 0, 0,
-            0, 0, .05, 0, 0, 0,
-            0, 0, 0, .05, 0, 0,
-            0, 0, 0, 0, .05, 0,
-            0, 0, 0, 0, 0, .05; // State Uncertainty
-        ukf.initialize(x, P);
-
+        ukf.initialize(x);
 
     for (int i = 0; i < 1; ++i) {
         Eigen::Vector3d gyro(0, 0, .01);
         Eigen::Vector3d accel(0, 0, -9.81);
-                   // Update and predict new states using measurement data and release update states
+        // Update and predict new states using measurement data and release update states
             x = ukf.update(x, .001, accel, gyro);
             qw = x(0);
             qx = x(1);
